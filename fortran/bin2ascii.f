e6286f38ba2237d179f406b5d9a6f33e32540b00
Jan Musinsky <musinsky@gmail.com>
2019-05-20 22:04:50 +0300

    add fortran

---
+++ fortran/bin2ascii.f
@@ -0,0 +1,32 @@
+c     2013-06-20
+
+      program bin2ascii
+      dimension a(1000)
+
+      open(1,file='dub57.bin',form='UNFORMATTED',status='OLD',
+     +     iostat=ios1)
+      if (ios1 .gt. 0) then
+         print *,'iostat = ',ios1
+         stop 1
+      endif
+      open(2,file='dub57.asc',status='REPLACE')
+
+      nev=0
+  100 read(1,end=200,err=300) nw,(a(i),i=1,nw)
+      if (nw .gt. 1000) then
+         print *,'skip event ',nev
+         goto 100
+      endif
+      write(2,*,err=300) nw,(a(i),i=1,nw)
+      nev=nev+1
+      goto 100
+
+  300 continue
+      print *,'error problem'
+  200 continue
+      print *,'end of file'
+      print *,'total events = ',nev
+      close(1)
+      close(2)
+      stop
+      end
---
+++ fortran/prc2dvo.f
@@ -0,0 +1,46 @@
+c     2002-10-23
+c     ascii -> bin
+
+      program prc_dvo
+
+c------perekodirovka dst->dvo------
+
+      character*12 DST_name, DVO_name
+      dimension a(1000)
+
+      print *
+      print *,' Perekodirovka DST -> DVO'
+      print *,' ------------------------'
+      print *
+
+      write(*,'(" Enter name of  input DST file : ",$)')
+      read *, DST_name
+      open ( 11, file=DST_name, status='old', iostat=ios1 )
+      if ( ios1 .gt. 0 ) then
+         print *,'iostat = ',ios1
+         stop 11
+      endif
+
+      write(*,'(" Enter name of output DVO file : ",$)')
+      read *, DVO_name
+      open ( 21 , file=DVO_name, form='unformatted',
+     +     iostat=ios2, status='new')
+      if ( DST_name .eq. DVO_name ) goto 200
+
+      nev=0
+    1 read ( 11, *,end=100,err=200 ) nw, (a(i), i=1,nw)
+      write (21, err=200 ) nw,(a(i),i=1,nw)
+      nev = nev + 1
+      go to 1
+
+  100 continue
+      print *
+      write (*,fmt=10) nev
+   10 format(3x,'Total number of event processing        nev=',i10)
+      stop
+
+  200 print *
+      print *,' Error on input or output file ...  '
+      print *
+      stop 33
+      end
