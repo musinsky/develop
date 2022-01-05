9a15f7957d410245ac901fa28451f567e662b9b4
Jan Musinsky <musinsky@gmail.com>
2022-01-05 17:31:38 +0100

    [disk_drive] additional info in gpt_part_info

--- disk_drive/gpt_part_info.sh
+++ disk_drive/gpt_part_info.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 
-# 2022-01-04
-# disk (only GPT) partitions info
+# 2022-01-05
 
-DEVICE="/dev/nvme0n1"   # no multiple devices
+# GPT disk partitions info (run as root or use sudo)
+# util-linux 2.35+ lsblk support all necessary columns to print
+# util-linux 2.36+ fdisk support -x, --list-details
+
+DEVICE="/dev/nvme0n1"   # or change to necessary (no multiple devices)
 if [ ! -e "$DEVICE" ]
 then
     DEVICE="/dev/sda"
@@ -15,13 +18,17 @@ NLINE="\n[root]#"
 UTILVER="rpm -qf $(which lsblk) $(which fdisk) $(which gdisk)"
 
 CMD1="lsblk -p -o NAME,SIZE,TYPE,FSTYPE,FSVER,LABEL,MOUNTPOINT,PTTYPE,PARTTYPE,PARTTYPENAME,PARTLABEL,PARTFLAGS $DEVICE"
-CMD2="fdisk -l $DEVICE"
-CMD3="fdisk -x $DEVICE"
-CMD4="gdisk -l $DEVICE"
-# header from "fdisk -x" and info from "fdisk -l"
+
+## fdisk -l => Device,Start,End,Sectors,Size,Type
+## fdisk -x => Device,Start,End,Sectors,Type-UUID,UUID,Name,Attrs
+#CMD2="fdisk -x -o Device,Start,End,Sectors,Size,Type,Type-UUID,Name,Attrs $DEVICE"
+## column 'Size' with "-x" print sizes in bytes and with "-l" in powers (MiB, GiB)
+
+# header from "-x" and info from "-l"
 CMD2A="fdisk -x $DEVICE | grep -B 999 '^$'"                              # before empty (new) line
 CMD2B="fdisk -l -o +Type-UUID,Name,Attrs $DEVICE | grep -A 999 'Device'" # after 'Device' word
 
+CMD3="gdisk -l $DEVICE"
 
 echo "# $(date -I)" > $FOUT
 echo -e "$NLINE $UTILVER" >> $FOUT
@@ -31,10 +38,8 @@ echo -e "$NLINE $CMD1" >> $FOUT
 $CMD1 >> $FOUT
 #echo -e "$NLINE $CMD2" >> $FOUT
 #$CMD2 >> $FOUT
-#echo -e "$NLINE $CMD3" >> $FOUT
-#$CMD3 >> $FOUT
 echo -e "$NLINE $CMD2A && $CMD2B" >> $FOUT
 (eval $CMD2A && eval $CMD2B) >> $FOUT
-echo -e "$NLINE $CMD4" >> $FOUT
-$CMD4 >> $FOUT
+echo -e "$NLINE $CMD3" >> $FOUT
+$CMD3 >> $FOUT
 echo -e "$NLINE" >> $FOUT
