dc3bfa72a97d591472712d7182438921c0492829
Jan Musinsky <musinsky@gmail.com>
2022-12-13 17:16:33 +0100

    [translit] more generate

--- translit/generate.ru.alphabet.sh
+++ translit/generate.ru.alphabet.sh
@@ -1,28 +1,105 @@
 #!/bin/bash
 
-# 2022-12-12
+# 2022-12-13
 
 # https://en.wikipedia.org/wiki/Cyrillic_script_in_Unicode
 # https://en.wikipedia.org/wiki/Russian_alphabet
 # subset of Cyrillic (U+0400..U+04FF)
 # subset of Russian alphabet U+0401,(U+0410..U+042F),(U+0430..U+044F),U+0451
-#                            CCL IO, CCL A.. CCL YA , CSL A.. CSL YA, CSL IO
+#                            CCL IO,(CCL A ..CCL YA),(CSL A ..CSL YA),CSL IO
+# modern Russian alphabet consists of 33 letters
 
-wget https://www.unicode.org/Public/UNIDATA/UnicodeData.txt
+wget -nc https://www.unicode.org/Public/UNIDATA/UnicodeData.txt
 
-URA='UnicodeData.ru.alphabet.txt'
-sed    -e '/^0000/,/^0400/d' -e '/^0402/,/^040F/d' UnicodeData.txt > "$URA"
-sed -i -e '/^0450/d'         -e '/^0452/,$d'       "$URA"
+UDRA='UnicodeData.ru.alphabet.txt'
+sed    -e '/^0000/,/^0400/d' -e '/^0402/,/^040F/d' UnicodeData.txt > "$UDRA"
+sed -i -e '/^0450/d'         -e '/^0452/,$d'       "$UDRA"
+sed -i -e 's/;[^;]*//2g' "$UDRA"
 
-sed -i -e 's/;[^;]*//2g' "$URA"
+# sort check (sorted by first word 'NNNN;CYRILLIC' as one word)
+LC_ALL=C sort "$UDRA" > "$UDRA.sort"
+diff "$UDRA" "$UDRA.sort" || { printf "not sorted\n"; exit 1; }
+rm "$UDRA.sort"
+# count check
+(( "$(wc --lines < $UDRA)" != 66 )) && { printf "not 2*33 letters\n"; exit 1; }
+printf "'%s' created\n" "$UDRA"
 
-LC_ALL=C sort "$URA" > "$URA.sort"
-diff "$URA" "$URA.sort" || { printf "not sorted \n"; exit 1; }
-rm "$URA.sort"
 
-if [ "$(wc --lines < $URA)" -ne "66" ]; then
-    printf "not 2*33 letters\n";
-    exit 1;
-fi
+# printf '\u044F'; printf '%b' '\u044F'
+# UNICODE_HEX="U+044F"; printf ${UNICODE_HEX/U+/"\U"}
 
-printf "'%s' created\n" "$URA"
+function print_info {
+    printf '%04d | U+%04X | %b\n' "$1" "$1" "$(printf '\\u%04X' "$1")"
+    (( "$1" == 0x042F )) && printf '\n'
+}
+function print_letter {
+    printf '%b\n' "$(printf '\\u%04X' "$1")"
+    (( "$1" == 0x042F )) && printf '\n'
+}
+function print_letter_row {
+    printf '%b' "$(printf '\\u%04X' "$1")"
+    need_space=1
+    (( "$1" == 0x042F )) && { printf '\n'; need_space=0; }
+    (( "$1" == 0x044F )) && { printf '\n'; need_space=0; }
+    (( "$need_space" == 1 )) && printf ' '
+}
+
+print_what="print_letter_row"
+for ((num = 0x0410; num <= 0x0415; num++)); do   # [А-Е]
+    $print_what "$num"
+done
+$print_what "0x0401"                             # Ё
+for ((num = 0x0416; num <= 0x042F; num++)); do   # [Ж-Я]
+    $print_what "$num"
+done
+for ((num = 0x0430; num <= 0x0435; num++)); do   # [а-е]
+    $print_what "$num"
+done
+$print_what "0x0451"                             # ё
+for ((num = 0x0436; num <= 0x044F; num++)); do   # [ж-я]
+    $print_what "$num"
+done
+
+
+## sort notes
+
+# man sort
+#
+# The locale specified by the environment affects sort order. Set LC_ALL=C to
+# get the traditional sort order that uses native byte values.
+
+# https://www.gnu.org/software/coreutils/manual/html_node/sort-invocation.html
+#
+# Unless otherwise specified, all comparisons use the character collating
+# sequence specified by the LC_COLLATE locale.
+#
+# Note that setting only LC_COLLATE has two problems. First, it is ineffective
+# if LC_ALL is also set. Second, it has undefined behavior if LC_CTYPE (or LANG,
+# if LC_CTYPE is unset) is set to an incompatible value. For example, you get
+# undefined behavior if LC_CTYPE is ja_JP.PCK but LC_COLLATE is en_US.UTF-8.
+#
+# ?! LC_ALL=C vs LC_ALL=C.UTF-8 vs CaseSensitive ?!
+
+# https://apple.stackexchange.com/q/335741
+#
+# When LC_COLLATE is defined as LC_COLLATE="en_US.UTF-8" on macOS, LC_COLLATE
+# uses the C or POSIX definiton. The default behavior of sort is to sort in
+# ascii order where uppercase has precedence over lowercase.
+#
+# On Linux, when LC_COLLATE="en_US.UTF-8" the LC_COLLATE definiton is
+# non-standard. The default behavior of sort is to ignore-case and lowercase has
+# precedence over uppercase.
+
+
+# https://stackoverflow.com/q/51202710
+#
+# You're looking for ordinal sorting, where characters are sorted numerically by
+# their Unicode code point ("ASCII value") and therefore all uppercase letters,
+# as a group, sort before all lowercase letters.
+#
+# As of Windows PowerShell Sort-Object uses lexical sorting, where
+# case-sensitive sorting simply means that the lowercase form of a given letter
+# comes directly before its uppercase form, not all letters collectively; e.g.,
+# b sorts before B, but they both come after both a and A (also, the logic is
+# reversed from the ordinal case, where it is uppercase letters that come
+# first).
