e34e37afcc9fb60d239234f24fdc17176ab24c3d
Jan Musinsky <musinsky@gmail.com>
2023-09-12 17:11:13 +0200

    [linux] more emacs batch info

--- linux/emacs01.cmd.sh
+++ linux/emacs01.cmd.sh
@@ -28,14 +28,17 @@ cat << EOF > "${TNAME}3.el"
 (message (format "arg[5]: %S" (nth 5 command-line-args)))
 (message (format "arg[6]: %S" (nth 6 command-line-args)))
 (message (format "arg[7]: %S" (nth 7 command-line-args)))
+(message (format "arg[8]: %S" (nth 8 command-line-args)))
+(message (format "arg[9]: %S" (nth 9 command-line-args)))
 
-(message (format "function arguments before: %S" command-line-args-left))
+(message (format "function arguments left before: %S" command-line-args-left))
 (let ((num1 (string-to-number (pop command-line-args-left)))
       (num2 (string-to-number (pop command-line-args-left))))
   ;;(setq c-basic-offset num1)
   (message "function numeric arguments: %d and %d" num1 num2))
 (message "function string argument:   %S" (pop command-line-args-left))
-(message (format "function arguments after:  %S" command-line-args-left))
+(message (format "function arguments left after:  %S" command-line-args-left))
+(message (format "function arguments all:  %s" command-line-args))
 
 (setq c-basic-offset (string-to-number (nth 4 command-line-args)))
 (indent-region (point-min) (point-max) nil)
@@ -44,11 +47,13 @@ EOF
 
 emacs --batch "${TNAME}1.c" -l "${TNAME}1.el" -f save-buffer
 emacs --batch "${TNAME}1.c" -l "${TNAME}2.el"
-emacs --batch "${TNAME}1.c" -l "${TNAME}3.el" 20 123 bla_bla
+# $ sh emacs01.cmd.sh a1 a2 a3
+emacs --batch "${TNAME}1.c" -l "${TNAME}3.el" 20 123 bla_bla "$@"
 
 # https://www.gnu.org/software/emacs/manual/html_node/emacs/Emacs-Invocation.html
 # https://www.gnu.org/software/emacs/manual/html_node/emacs/Initial-Options.html
 # https://www.gnu.org/software/emacs/manual/html_node/emacs/Command-Example.html
+# https://www.gnu.org/software/emacs/manual/html_node/elisp/Command_002dLine-Arguments.html
 
 # --batch
 # Run Emacs in batch mode. Batch mode is used for running programs written in
