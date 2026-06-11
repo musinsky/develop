afbcde7b42fcafa73a0606372beadb9d7a90e909
Jan Musinsky <musinsky@gmail.com>
2026-06-11 22:26:42 +0200

    [alice] small improvements

--- alice/root_dict_GRPLHCIFData/compile.custom.dict.sh
+++ alice/root_dict_GRPLHCIFData/compile.custom.dict.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-# 2026-05-25
+# 2026-06-11
 # shellcheck disable=SC2046
 
-NAME='GRPLHCIFData'
-HDR_DIR='AliceO2_GRPLHCIFData'
+SOURCE_DIR="$(realpath AliceO2_GRPLHCIFData)"
+OUT_DIR="."
+# OUT_DIR="${SOURCE_DIR}"
+OUT_NAME="GRPLHCIFData"
 
-rootcling -f "${NAME}_dict.cxx" "$HDR_DIR/GRPLHCIFData.h" LinkDef.h
-g++ -fPIC -shared -O0 "${NAME}_dict.cxx" -o "lib${NAME}.only.dict.so" $(root-config --cflags --libs)
+rootcling -f "${OUT_DIR}/dict_${OUT_NAME}.cxx" \
+          -I "${SOURCE_DIR}" GRPLHCIFData.h LinkDef.h
+g++ -fPIC -shared -O0 "${OUT_DIR}/dict_${OUT_NAME}.cxx" -I "${SOURCE_DIR}" \
+    -o "${OUT_DIR}/lib${OUT_NAME}.only.dict.so" $(root-config --cflags --libs)
 
-# rm "${NAME}_dict.cxx" "${NAME}_dict_rdict.pcm"
+# -O0 is default gcc optimization option
+# rm "${OUT_DIR}/dict_${OUT_NAME}.cxx" "${OUT_DIR}/dict_${OUT_NAME}_rdict.pcm"
+# rm "${OUT_DIR}/lib${OUT_NAME}.only.dict.so"
