b62531dedb3d986470fb91b92632d5571f0a82bb
Jan Musinsky <musinsky@gmail.com>
2026-06-17 14:00:34 +0200

    [alice] using unified functions

--- alice/root_dict_GRPLHCIFData/test.custom.dict.C
+++ alice/root_dict_GRPLHCIFData/test.custom.dict.C
@@ -1,4 +1,4 @@
-// 2026-06-12
+// 2026-06-17
 
 // root [0] TFile f("o2-parameters-GRPLHCIFData_1778951539107.root")
 // Warning in <TClass::Init>: no dictionary for class o2::parameters::GRPLHCIFData is available
@@ -75,4 +75,65 @@ void test_custom_dict()
   // const auto& rBCPattern = rBunchFilling.getBCPattern();
   // for (size_t i = 0; i < rBCPattern.size(); i++) { if (rBCPattern.test(i)) std::cout << i << std::endl; }
   // for (size_t i = 0; i < 70; i++)                { if (rBCPattern.test(i)) std::cout << i << std::endl; }
+
+  // https://github.com/AliceO2Group/AliceO2/blob/dev/Common/Constants/include/CommonConstants/LHCConstants.h
+  // https://github.com/AliceO2Group/AliceO2/blob/dev/DataFormats/common/include/CommonDataFormat/BunchFilling.h
+  // getPattern(-1) = getBCPattern()    = pattern of interacting BCs at P2, -1 for InteractingBC
+  // getPattern(0)  = getBeamPattern(0) = pattern of beam bunches at P2, 0 for A beam (from ATLAS side, clockwise)
+  // getPattern(1)  = getBeamPattern(1) = pattern of beam bunches at P2, 1 for C beam (from CMS side, anticlockwise)
+  //
+  // testBC(bcID, -1) = testInteractingBC(bcID) = test interacting (-1) beams bunch    // Interacting
+  // testBC(bcID, 0)  = testBeamBunch(bcID, 0)  = test clockwise (0, A) beam bunch     // Beam-A
+  // testBC(bcID, 1)  = testBeamBunch(bcID, 1)  = test anticlockwise (1, C) beam bunch // Beam-C
+
+  // const std::bitset<o2::constants::lhc::LHCMaxBunches>& rBCPattern2 = rBunchFilling.getPattern(-1);
+  // const std::bitset<o2::constants::lhc::LHCMaxBunches>& rBeamAPattern = rBunchFilling.getPattern(0);
+  // const std::bitset<o2::constants::lhc::LHCMaxBunches>& rBeamCPattern = rBunchFilling.getPattern(1);
+  std::cout << std::endl;
+  std::cout << std::format("Interacting beams, number of bunches: {}\n", rBunchFilling.getNBunches(-1));
+  std::cout << std::format("Beam-A, number of bunches:            {}\n", rBunchFilling.getNBunches(0));
+  std::cout << std::format("Beam-C, number of bunches:            {}\n", rBunchFilling.getNBunches(1));
+  std::cout << "       I A C" << std::endl;
+  for (size_t i = 90; i < 110; i++) {
+    std::cout << std::format("[{:4}] ", i);
+    if (rBunchFilling.testBC(i, -1)) {
+      std::cout << "1 ";
+    } else {
+      std::cout << "0 ";
+    }
+    if (rBunchFilling.testBC(i, 0)) {
+      std::cout << "1 ";
+    } else {
+      std::cout << "0 ";
+    }
+    if (rBunchFilling.testBC(i, 1)) {
+      std::cout << "1";
+    } else {
+      std::cout << "0";
+    }
+    std::cout << std::endl;
+  }
 }
+
+// ### WARNING ###
+// https://en.cppreference.com/cpp/utility/bitset
+//
+// [] std::bitset<5> bits{"10110"}
+// [] cout << bits << std::endl
+// 10110   // !!!
+// [] cout << bits[0] << bits[1] << bits[2] << bits[3] << bits[4] << std::endl
+// 01101   // !!!
+//
+// bits[0] is LSB (least significant bit)
+// std::bitset<> is not like a "character array", it is like a "binary number"
+//
+// [] std::bitset<5> Bits{}
+// [] Bits.set(1, true)           // use set() function to avoid mistake, confusion
+// [] cout << Bits << std::endl
+// 00010
+// [] cout << Bits[0] << Bits[1] << Bits[2] << Bits[3] << Bits[4] << std::endl
+// 01000
+// [] Bits.test(0)                // use test() function to avoid mistake, confusion
+// (bool) false
+// [] Bits.test(1)                // use test() function to avoid mistake, confusion
+// (bool) true
